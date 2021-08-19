f29e88828b32640c321e281c23bfdcfbd56b0fdc
sasha52205 <sashakazancev111@gmail.com>
2021-08-19 22:22:45 +0500

    update2

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from utils.set_bot_commands import set_default_commands
 from loader import db, scheduler
 from utils.db_api import db_gino, quick_commands
 
-async def drop_today
+#async def drop_today
 
-def scheduler_jobs():
-    scheduler.add_job(drop_today, 'cron', day_of_week='0-6',hour=23, minute=58)
+#def scheduler_jobs():
+#    scheduler.add_job(drop_today, 'cron', day_of_week='0-6',hour=23, minute=58)
 
 async def on_startup(dp):
     import filters
@@ -27,12 +27,12 @@ async def on_startup(dp):
 
     print("Создаем таблицы")
     await db.gino.create_all()
-    # await quick_commands.add_link(1, "@samatrener")
+    await quick_commands.add_link(1, "@clubhchat")
     print("Готово")
-    await on_startup_notify(dp)
+  #  await on_startup_notify(dp)
     await set_default_commands(dp)
     await test()
-    scheduler.jobs()
+ #   scheduler.jobs()
 
 
 
@@ -40,6 +40,8 @@ if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    scheduler.start()
+   # scheduler.start()
 
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+
+
